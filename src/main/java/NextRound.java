7512cd7e6bb114a0c14e1de4b48cc40e994ee0e0
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:28:11 -0500

    158A - Next Round

---
+++ src/main/java/NextRound.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NextRound {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> arrayList = new ArrayList<>();
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int numberOfParticipants = 0;
+
+        for (int i = 0; i < n; i++) {
+            int number = scanner.nextInt();
+            arrayList.add(number);
+        }
+        if(k<=arrayList.size()) {
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (arrayList.get(i)>0 && (arrayList.get(i) >= arrayList.get(k-1))) {
+                    numberOfParticipants++;
+                }
+            }
+        }
+        System.out.println(numberOfParticipants);
+    }
+}
