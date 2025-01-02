75c0930bc295954696ffc87d7e15164d59c854ae
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:42:04 -0500

    41A - Translation

---
+++ src/main/java/Translation.java
@@ -0,0 +1,20 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Translation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        String finalWord = scanner.nextLine();
+        String result = "";
+
+        for (int i = word.length()-1; i >=0 ; i--) {
+            result += word.charAt(i)+"";
+        }
+        if(Objects.equals(finalWord, result)){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
