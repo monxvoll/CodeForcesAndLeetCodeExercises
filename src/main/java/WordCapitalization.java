81d746cb7c655eb4c4397233e4d6c2e4e2d9d6e3
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:44:08 -0500

    281A - Word Capitalization

---
+++ src/main/java/WordCapitalization.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class WordCapitalization {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        String newWord = word.replaceFirst(word.charAt(0)+"",(word.charAt(0)+"").toUpperCase());
+
+        System.out.println(newWord);
+    }
+}
