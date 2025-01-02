ff3e1fb20cfcd25a8379a336d0e60839744f25d0
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:10:44 -0500

    791A - Bear and Big Brother

---
+++ src/main/java/BearAndBigBrother.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BearAndBigBrother {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int limakWeight = scanner.nextInt();
+        int bobWeight = scanner.nextInt();
+        int yearsNum = 0;
+
+        while (limakWeight<=bobWeight){
+            limakWeight *= 3;
+            bobWeight *= 2;
+            yearsNum++;
+        }
+        System.out.println(yearsNum);
+    }
+}
