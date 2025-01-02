c3155f2ef1301a3ff7b26ffa716ce1cb27979834
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:34:31 -0500

    266A - Stones on the Table

---
+++ src/main/java/StonesOnTheTable.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StonesOnTheTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nStones = scanner.nextInt();
+        scanner.nextLine();
+        String stones = scanner.nextLine();
+        int mStones = 0;
+        
+        for (int i = 0; i < nStones-1; i++) {
+            if(stones.charAt(i)==stones.charAt(i+1)){
+                mStones++;
+            }
+        }
+        System.out.println(mStones);
+    }
+}
+
+
+
