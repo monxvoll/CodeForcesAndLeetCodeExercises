c2976291a3b1b8ad697ce41b39649232c2dda3c6
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:23:05 -0500

    318A - Even Odds

---
+++ src/main/java/EvenOdds.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class EvenOdds {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Long integerN;
+        Long integerK;
+        integerN = scanner.nextLong();
+        integerK = scanner.nextLong();
+        Long odd = (integerN+1)/2;
+
+        if(integerK>odd){
+            System.out.println(""+(integerK-odd)*2);
+        }else {
+            System.out.println(""+((2*integerK)-1));
+        }
+    }
+}
