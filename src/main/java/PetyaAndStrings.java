b7be74b2908795780cb67bd113860d43ac35f6f5
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:32:24 -0500

    112A - Petya and Strings

---
+++ src/main/java/PetyaAndStrings.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PetyaAndStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstWord = scanner.nextLine().toLowerCase();
+        String secondWord = scanner.nextLine().toLowerCase();
+
+        for (int i = 0; i < firstWord.length() ; i++) {
+            if (firstWord.charAt(i)<secondWord.charAt(i)){
+                System.out.println(-1);
+                System.exit(0);
+            }else if(firstWord.charAt(i)>secondWord.charAt(i)){
+                System.out.println(1);
+                System.exit(0);
+            }
+        }
+
+        System.out.println(0);
+    }
+}
