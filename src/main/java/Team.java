f2d308b9df05d34a2a5e7520582d724a5fbe4e33
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:36:48 -0500

    231A - Team

---
+++ src/main/java/Team.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Team {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int contestNumber,petyaView,vasyasView,tonyaView,problemsNumber = 0;
+        contestNumber = scanner.nextInt();
+        
+        for (int i = 0; i < contestNumber ; i++) {
+            petyaView = scanner.nextInt();
+            vasyasView = scanner.nextInt();
+            tonyaView = scanner.nextInt();
+            if((petyaView+vasyasView+tonyaView)>=2){
+                problemsNumber++;
+            }
+        }
+        System.out.println(problemsNumber);
+    }
+}
