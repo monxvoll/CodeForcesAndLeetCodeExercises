cf1502d80d627a748e9df9658a561b6a73069bed
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:43:09 -0500

    71A - Way Too Long Words

---
+++ src/main/java/WayTooLongWords.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class WayTooLongWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<String> wordsList = new ArrayList<>();
+        scanner.nextLine();
+
+        for (int i = 0; i < n ; i++) {
+            String word = scanner.nextLine();
+            if(word.length()<=10){
+                wordsList.add(word);
+            }else{
+                String  mediumLetter =  word.length()-2+"";
+                word = word.charAt(0) + mediumLetter + word.charAt(word.length()-1);
+                wordsList.add(word);
+            }
+        }
+        for (String string : wordsList){
+            System.out.println(string);
+        }
+    }
+}
