685667c1778f1f8e24f4520162dcf4ed060bf5fb
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:12:39 -0500

    282A - Bit++

---
+++ src/main/java/Bit.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bit{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int statementsNum = scanner.nextInt();
+        scanner.nextLine();
+        int xValue = 0;
+
+        for (int i = 0; i < statementsNum; i++) {
+            String statement = scanner.nextLine();
+            if(statement.contains("-")){
+                xValue --;
+            }else if (statement.contains("+")) {
+                xValue ++;
+            }
+        }
+        System.out.println(xValue);
+    }
+}
