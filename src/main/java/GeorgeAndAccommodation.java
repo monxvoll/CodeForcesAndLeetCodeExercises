c58f7c4bf408d849a5ca7275658b069bc32756a0
monx.voll <stedy3348112688@gmail.com>
2025-01-02 10:25:09 -0500

    467A - George and Accommodation

---
+++ src/main/java/GeorgeAndAccommodation.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GeorgeAndAccommodation {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            Long totalRooms = scanner.nextLong();
+            int temp = 0;
+
+            for (int i = 0; i <totalRooms ; i++) {
+                Long actualPeople = scanner.nextLong();
+                Long limitPeople = scanner.nextLong();
+                if((limitPeople-actualPeople) >= 2){
+                    temp++;
+
+                }
+            }
+            System.out.print(temp);
+    }
+}
