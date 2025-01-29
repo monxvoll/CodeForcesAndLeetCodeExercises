77905c6caa40903c08000cc688d49adec6bc9826
monx.voll <stedy3348112688@gmail.com>
2025-01-29 17:54:14 -0500

    LeetCode exercises

---
+++ src/main/java/leetcode/FindContentChildren.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class FindContentChildren {
+//455. Assign Cookies
+    static class Solution {
+        public int findContentChildren(int[] g, int[] s) {
+            Arrays.sort(s); Arrays.sort(g);
+            int n = 0;
+            for (int i = 0; i < s.length; i++) {
+                for ( int j = n; j < g.length; j++) {
+                    if(s[i]>=g[j]){
+                        n++;
+                        break;
+                    }
+                }
+            }
+            return n;
+        }
+    }
+}
