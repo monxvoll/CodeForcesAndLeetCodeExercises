9ea6359a111f40923621aa0d05ecf5703a4446d8
monx.voll <stedy3348112688@gmail.com>
2025-02-06 22:26:46 -0500

    LeetCode Exercises

---
+++ src/main/java/leetcode/IsomorphicStrings.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.*;
+
+public class IsomorphicStrings {
+    //205. Isomorphic Strings
+    static class Solution {
+        public  boolean isIsomorphic(String s, String t) {
+            Map<Character, Character> map = new HashMap<>();
+            Set<Character> usedValues = new HashSet<>();
+            for (int i = 0; i <s.length() ; i++) {
+                if(map.containsKey(s.charAt(i))){
+                    if(map.get(s.charAt(i))!=t.charAt(i)){
+                        return false;
+                    }
+                }else {
+                    if(!usedValues.add(t.charAt(i))){
+                        return false;
+                    }
+                    map.put(s.charAt(i),t.charAt(i));
+                }
+            }
+            return true;
+        }
+    }
+}
