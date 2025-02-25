469a83ce26e4d1ecb6c683161a9e3379fb225c0f
monx.voll <stedy3348112688@gmail.com>
2025-02-24 21:47:51 -0500

    LeetCode Package

---
+++ src/main/java/leetcode/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+package leetcode;
+public class RemoveDuplicates {
+    //26. Remove Duplicates from Sorted Array
+
+    static class Solution {
+        public  int removeDuplicates(int[] nums) {
+            int k = 1;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != nums[i - 1]) {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+}
